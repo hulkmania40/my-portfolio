0c15493d9bf17a223556b4c8a83f71329b25266b
Mubashir Ali <62755362+hulkmania40@users.noreply.github.com>
2024-07-14 10:56:13 +0000

    Add Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Myprojects from './components/myprojects/Myprojects';
-import Contacts from './components/contacts/Contacts';
-import Footer from './components/footer/Footer';
-import { Toaster } from 'react-hot-toast';
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Myprojects from "./components/myprojects/Myprojects";
+import Contacts from "./components/contacts/Contacts";
+import Footer from "./components/footer/Footer";
+import { Toaster } from "react-hot-toast";
+import ReactGA from "react-ga4";
 
 const App: React.FC = () => {
+  ReactGA.initialize("G-4QJQTB4F1M");
   return (
     <>
-      <Toaster/>
+      <Toaster />
       <Header />
       <Nav />
       <About />
@@ -21,6 +23,6 @@ const App: React.FC = () => {
       <Footer />
     </>
   );
-}
+};
 
 export default App;
